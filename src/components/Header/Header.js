fa5efc1842d9db4d73b7caf13ac8409ff23e825d
Inder <redni1920@gmail.com>
2020-09-22 13:00:03 +0200

    Add structure and styles for Header component
    Create utility class .button in utility modules to display links as buttons.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,15 @@ import './styles.scss';
 
 const Header = () => {
   return (
-    <header>
-      Header
+    <header className="header">
+      <a href="#" className="logo">
+        <img src="/images/logo.png" />
+      </a>
+      <nav>
+        <a href="#" className="button">vergliech</a>
+        <a href="#" className="button">standorte</a>
+        <a href="#" className="button">impressum</a>
+      </nav>
     </header>
   );
 };
