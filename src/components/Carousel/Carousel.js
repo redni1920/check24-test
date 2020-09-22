1ae2068950e40f59069954e95dc8cd7145715ef4
Inder <redni1920@gmail.com>
2020-09-22 14:10:12 +0200

    Add Carousel component
    Create Carousel component and add desktop styles

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import './styles.scss';
+
+const Carousel = ({ slides }) => {
+  const [activeSlide, updateIndex] = useState(0);
+
+  const onPrev = () => {
+    const newIndex = activeSlide === 0 ? slides.length - 1 : activeSlide - 1;
+    updateIndex(newIndex);
+  };
+
+  const onNext = () => {
+    const newIndex = (activeSlide + 1) % slides.length;
+    updateIndex(newIndex);
+  };
+
+  return (
+    <div className="carousel">
+      <div className="slide">
+        <img src={slides[activeSlide]} />
+        <h2 className="legend">Lorem ipsum dolor sit amet</h2>
+      </div>
+      <ul className="slide-buttons">
+        <li>
+          <button onClick={onPrev}>Prev</button>
+        </li>
+        <li>
+          <button onClick={onNext}>Next</button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Carousel;
